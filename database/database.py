bdd0a4361ea51fb29c9307daea1562935d24341d
leggacys <bogatu.daniel21@gmail.com>
2026-08-10 20:58:17 +0200

    Use psycopg3 database driver

--- database/database.py
+++ database/database.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -13,15 +12,20 @@ if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is missing")
 
 
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+psycopg://",
+        1,
+    )
+
 class Base(DeclarativeBase):
     pass
-
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
 )
-
 
 SessionLocal = sessionmaker(
     bind=engine,
